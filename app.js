eda7c59109cec5086880fe7c33a298702aeea785
RashmithaNGowda <rash40smg@gmail.com>
2021-10-15 00:28:24 -0700

    pathresolve

--- app.js
+++ app.js
@@ -1,19 +1,2 @@
-const button = document.querySelector('button')
-const input=document.querySelector('input')
-const div=document.querySelector('div')
-
-// div.innerHTML=localStorage.getItem('InputValue')
-// // console.log(button,input,div)
-// button.addEventListener('click',(req,res)=>{
-//     localStorage.setItem('InputValue',input.value)
-//     div.innerHTML=input.value
-
-// })
-const arr=[1,2,3,4,5]
-
-// console.log(arr)
-// console.log(val)
-localStorage.setItem('val',JSON.stringify(arr))
-console.log(JSON.parse(localStorage.getItem('val')))
-
-
+const path=require('path')
+console.log(path.resolve('./app.js'))
