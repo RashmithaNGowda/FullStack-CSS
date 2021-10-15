b4cd9db7c5650ee3410dc2040df2c6cf40d9ab81
RashmithaNGowda <rash40smg@gmail.com>
2021-10-15 13:17:16 -0700

    fs readFile func

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-const path=require('path')
-console.log(path.extname('./app.js'))
-console.log(path.dirname('./temp/utility.js'))
-console.log(__dirname)
-console.log(path.join(__dirname,'./app.js'))
-console.log(path.resolve('./app.js'))
+const fs =require('fs')
+
+fs.readFile('./test.txt',(error,data)=>{
+    if(error)console.log(error)
+    else console.log(typeof data)
+  
+})
