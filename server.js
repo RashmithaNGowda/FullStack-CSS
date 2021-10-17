ccc14117ad7a5137ef335c1c6c8b500853175740
RashmithaNGowda <rash40smg@gmail.com>
2021-10-16 18:20:50 -0700

    use in express

--- server.js
+++ server.js
@@ -1,33 +1,13 @@
 const express=require('express')
 const PORT=3000
 const app = express()
-// console.log(express)
 
-// const verify=(req,res,next)=>{
-//     if(req.headers['user-agent']==="Thunder Client (https://www.thunderclient.io)") next()
-//     else res.send(" sorryy!! BLOCKED")
-// }
+app.use(express.json())
 
-app.get('/public',(req,res)=>{
-    res.send("This is PUBLIC ROUTE")
+app.post('/signup',(req,res)=>{
+    console.log(req.body)
+    res.send('dadada')
 })
-// authentication--------
-const isAdmin=(req,res,next)=>{
-    if(req.headers.admin==="true")next()
-    else res.send("Sorry bruh! Not authorised Private Route")
-
-}
-
-app.get('/private',isAdmin,(req,res)=>{
-    res.send("Authorized")
-})
-
-// app.get('/',verify,(req,res)=>{
-//     // console.log(req.headers.host)
-//     res.send(" GET it bruh")
-//     // console.log(req)
- 
-// })
 
 app.listen(3000,()=>{
     console.log(`Server listening at Port : ${PORT}`)
