f31e06dbfcd86ee683264affaee483c0983a5d91
RashmithaNGowda <rash40smg@gmail.com>
2021-10-31 18:29:35 -0700

    react and react DOM-day34

---
+++ app.js
@@ -0,0 +1,6 @@
+const container = document.getElementById("app")
+
+// console.log(container)
+const content =  React.createElement('h2',{},"This is a heading")
+
+ReactDOM.render(content,container)
