3b3d5f3180011b2e3e0c6491dadb7a9988af86e6
RashmithaNGowda <rash40smg@gmail.com>
2021-12-02 14:11:43 -0800

    Day39

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,17 +1,37 @@
-import Content from "./components/Content";
-import Footer from "./components/layouts/Footer";
-import Navbar from "./components/layouts/Navbar";
+import { useState, useEffect } from 'react'
+import Calculator from './components/Calculator';
 
-function App() {
+import Navbar from './layouts/Navbar'
+import Info from './components/Info';
+import Timer from './components/Timer';
+import Tap from './components/Tap'
+
+   
+const App = () => {
+  // const currentComp = 'calculator'
+  const [currentComp, setcurrentComp] = useState('info')
+
+  // if (currentComp === 'calculator') return <Calculator />
+  // else if (currentComp === 'timer') return <Timer />
+  // else if (currentComp === 'tap') return <Tap />
+  // else return <Info />
+
   return (
     <div className="App">
-      {/* <Navbar /> */}
-      <Content />
-     
-
-      {/* <Footer /> */}
-    </div>
+      <Navbar setcurrentComp={setcurrentComp} />
+      {
+        currentComp === 'calculator' ? <Calculator />
+          : currentComp === "timer" ? <Timer />
+            : currentComp === 'tap' ? <Tap />
+              : <Info />
+      }
+      <Info />
+      <Timer />
+      <Calculator />
+      <Tap />
+    </div >
   );
 }
 
 export default App;
+
